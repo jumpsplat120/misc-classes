bc7b33ce693784b1ac0ed17addd65f00d9bfbdf6
jumpsplat120 <jumpsplat120@yahoo.com>
2026-05-04 15:31:34 -0700

    Create Symbol.d.lua

---
+++ documentation/Symbol.d.lua
@@ -0,0 +1,12 @@
+---@meta
+
+---Creates a symbol. While passing an `id` makes the symbol more human readable, it's not required.
+---
+---If no `id` is passed, then it will attempt to call `math.uuid`, if it exists. If it does not, then it
+---simply generates a random float between zero and one to use as the id.
+---@class Symbol.Class
+---@overload fun(id: any): Symbol
+
+---A way to have a non unique value, like a string, represented by something unique.
+---@class Symbol : Classy.Object
+---@field id string Entirely meant for human readability, and otherwise has no function.
