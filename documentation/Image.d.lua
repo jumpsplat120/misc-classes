9c56732868c82a804cda4e6cf11c4d2faf45534a
jumpsplat120 <jumpsplat120@yahoo.com>
2026-05-05 03:48:18 -0700

    Update Image.d.lua (WIP)

--- documentation/Image.d.lua
+++ documentation/Image.d.lua
@@ -1,3 +1,19 @@
 ---@meta Image
 
 ---@class Image.Class : Classy.Object, Drawable.Mixin
+---@field size Vector.Class
+---@field offset Vector.Class
+Image = {}
+
+---@param image any
+---@param x any
+---@param y any
+function Image:fromValues(image, x, y) end
+
+function Image:fromVector(image, position) end
+
+function Image:new(opts) end
+
+function Image:draw() end
+
+function Image:isVisible(...) end
