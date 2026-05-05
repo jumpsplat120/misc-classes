273e3436d7506a57cd13b2090f5482eb26943908
jumpsplat120 <jumpsplat120@yahoo.com>
2026-05-04 20:12:58 -0700

    Create Drawable.d.lua

---
+++ documentation/Drawable.d.lua
@@ -0,0 +1,17 @@
+---@meta Drawable
+
+---A mixin that adds [color](lua://Color.Class) and a [transform](lua://Transform.Class) to an object. The object
+---will still need to define it's own specific drawing logic.
+---@class Drawable.Mixin
+---@field color Color.Class A getter for the [color](lua://Color.Class) object. No setter exists for this field.
+---@field transform Transform.Class A getter for the [transform](lua://Transform.Class) object. No setter exists for this field.
+Drawable = {}
+
+---Creates the entries in [private](lua://Classy.private) needed for Drawable to function. Call in it's parent class
+---with `Drawable.new(self)`.
+function Drawable:new() end
+
+---Method meant to be called interally, which applies the [color](lua://Color.Class) and
+---[transform](lua://Transform.Class).
+---@protected
+function Drawable:drawable() end
