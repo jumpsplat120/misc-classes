fe160c3d21a5f8caa383fbf65618594d58a1e591
jumpsplat120 <jumpsplat120@yahoo.com>
2026-05-05 03:44:23 -0700

    Add type, add fallback

--- mixins/Unpack.lua
+++ mixins/Unpack.lua
@@ -4,10 +4,11 @@ local Unpack
 Object  = require("lib.Classy")
 private = require("lib.Classy.instances")
 
+---@type Unpack.Mixin
 Unpack = Object:init()
 
 function Unpack:unpack()
-    return table.unpack(private[self].values)
+    return table.unpack(private[self].values or {})
 end
 
 return Unpack
