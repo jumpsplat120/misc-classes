3d10e5cff4693be55c0c7a819dadc5a58fc1ab8e
jumpsplat120 <jumpsplat120@yahoo.com>
2026-05-05 03:43:46 -0700

    Update logic so that the coroutine doesn't error after the first full loop; add method version of iterator

--- mixins/Ipairs.lua
+++ mixins/Ipairs.lua
@@ -4,12 +4,69 @@ local Ipairs
 Object  = require("lib.Classy")
 private = require("lib.Classy.instances")
 
+---@type Ipairs.Mixin
 Ipairs = Object:init()
 
-Ipairs.__call = coroutine.wrap(function(self)
-    for i, v in ipairs(private[self].values) do
-        coroutine.yield(i, v)
+    --======PRIVATE FUNCTIONS======--
+
+    --======CONSTRUCTOR======--
+
+function Ipairs:new()
+    private[self].ipairs = {}
+end
+
+    --======STATIC======--
+
+    --======METHODS======--
+
+function Ipairs:ipairs()
+    local p = private[self].ipairs
+    
+    if p.coroutine and coroutine.status(p.coroutine) == "dead" then
+        p.coroutine = nil
     end
-end)
+
+    if not p.coroutine then
+        p.coroutine = coroutine.create(function()
+            for i, v in ipairs(private[self].values or {}) do
+                coroutine.yield(i, v)
+            end
+        end)
+    end
+
+    return function()
+        local status, i, v = coroutine.resume(p.coroutine)
+
+        return i, v
+    end
+end
+
+    --======GETTERS======--
+
+    --======SETTERS======--
+
+    --======METAMETHODS======--
+
+function Ipairs:__call()
+    local p, status, i, v
+    
+    p = private[self].ipairs
+    
+    if p.coroutine and coroutine.status(p.coroutine) == "dead" then
+        p.coroutine = nil
+    end
+
+    if not p.coroutine then
+        p.coroutine = coroutine.create(function()
+            for i, v in ipairs(private[self].values or {}) do
+                coroutine.yield(i, v)
+            end
+        end)
+    end
+
+    status, i, v = coroutine.resume(p.coroutine)
+        
+    return i, v
+end
 
 return Ipairs
--- mixins/Pairs.lua
+++ mixins/Pairs.lua
@@ -4,14 +4,69 @@ local Pairs
 Object  = require("lib.Classy")
 private = require("lib.Classy.instances")
 
+---@type Pairs.Mixin
 Pairs = Object:init()
 
-Pairs.__call = coroutine.wrap(function(self)
-    local values = private[self].values or {}
+    --======PRIVATE FUNCTIONS======--
 
-    for k, v in pairs(values) do
-        coroutine.yield(k, v)
+    --======CONSTRUCTOR======--
+
+function Pairs:new()
+    private[self].pairs = {}
+end
+
+    --======STATIC======--
+
+    --======METHODS======--
+
+function Pairs:pairs()
+    local p = private[self].pairs
+    
+    if p.coroutine and coroutine.status(p.coroutine) == "dead" then
+        p.coroutine = nil
     end
-end)
+
+    if not p.coroutine then
+        p.coroutine = coroutine.create(function()
+            for k, v in pairs(private[self].values or {}) do
+                coroutine.yield(k, v)
+            end
+        end)
+    end
+
+    return function()
+        local status, k, v = coroutine.resume(p.coroutine)
+
+        return k, v
+    end
+end
+
+    --======GETTERS======--
+
+    --======SETTERS======--
+
+    --======METAMETHODS======--
+
+function Pairs:__call()
+    local p, status, k, v
+    
+    p = private[self].pairs
+    
+    if p.coroutine and coroutine.status(p.coroutine) == "dead" then
+        p.coroutine = nil
+    end
+
+    if not p.coroutine then
+        p.coroutine = coroutine.create(function()
+            for k, v in pairs(private[self].values or {}) do
+                coroutine.yield(k, v)
+            end
+        end)
+    end
+
+    status, k, v = coroutine.resume(p.coroutine)
+        
+    return k, v
+end
 
 return Pairs
