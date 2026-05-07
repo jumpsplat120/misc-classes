c11f33b8cd907b1456f0e145422f257f8a217501
jumpsplat120 <jumpsplat120@yahoo.com>
2026-05-07 03:18:03 -0700

    Update mixin docs using generics so that we get the correct return value for parent object

--- documentation/mixins/MouseInteractions.d.lua
+++ documentation/mixins/MouseInteractions.d.lua
@@ -13,44 +13,53 @@ function MouseInteractions:new() end
 
 ---Hook that must be called within [love.update](lua://love.update). *All* params from the love callback should be
 ---passed through, as they are used for calculation, then passed through to relevant dispatched events.
+---@generic T
+---@param self T
 ---@param dt number Time since the last update in seconds.
----@return self
-function MouseInteractions:update(dt) end
+---@return T
+function MouseInteractions.update(self, dt) end
 
 ---Hook that must be called within [love.mousepressed](lua://love.mousepressed). *All* params from the love callback
 ---should be passed through, as they are used for calculation, then passed through to relevant dispatched events.
+---@generic T
+---@param self T
 ---@param x number Mouse x position, in pixels.
 ---@param y number Mouse y position, in pixels.
 ---@param button number The button index that was pressed. 1 is the primary mouse button, 2 is the secondary mouse button, and 3 is the middle button. Further buttons are mouse dependent.
 ---@param istouch boolean True if the mouse button press originated from a touchscreen touch-press.
 ---@param presses number The number of presses in a short time frame and small area, used to simulate double, triple clicks.
----@return self
-function MouseInteractions:mousepressed(x, y, button, istouch, presses) end
+---@return T
+function MouseInteractions.mousepressed(self, x, y, button, istouch, presses) end
 
 ---Hook that must be called within [love.mousereleased](lua://love.mousereleased). *All* params from the love callback
 ---should be passed through, as they are used for calculation, then passed through to relevant dispatched events.
+---@generic T
+---@param self T
 ---@param x number Mouse x position, in pixels.
 ---@param y number Mouse y position, in pixels.
 ---@param button number The button index that was pressed. 1 is the primary mouse button, 2 is the secondary mouse button, and 3 is the middle button. Further buttons are mouse dependent.
 ---@param istouch boolean True if the mouse button press originated from a touchscreen touch-press.
 ---@param presses number The number of presses in a short time frame and small area, used to simulate double, triple clicks.
----@return self
-function MouseInteractions:mousereleased(x, y, button, istouch, presses)
-end
+---@return T
+function MouseInteractions.mousereleased(self, x, y, button, istouch, presses) end
 
 ---Hook that must be called within [love.mousemoved](lua://love.mousemoved). *All* params from the love callback
 ---should be passed through, as they are used for calculation, then passed through to relevant dispatched events.
+---@generic T
+---@param self T
 ---@param x number The mouse position on the x-axis.
 ---@param y number The mouse position on the y-axis.
 ---@param dx number The amount moved along the x-axis since the last time love.mousemoved was called.
 ---@param dy number The amount moved along the y-axis since the last time love.mousemoved was called.
 ---@param istouch boolean True if the mouse button press originated from a touchscreen touch-press.
----@return self
-function MouseInteractions:mousemoved(x, y, dx, dy, istouch) end
+---@return T
+function MouseInteractions.mousemoved(self, x, y, dx, dy, istouch) end
 
 ---Hook that must be called within [love.wheelmoved](lua://love.wheelmoved). *All* params from the love callback
 ---should be passed through, as they are used for calculation, then passed through to relevant dispatched events.
+---@generic T
+---@param self T
 ---@param x number Amount of horizontal mouse wheel movement. Positive values indicate movement to the right.
 ---@param y number Amount of vertical mouse wheel movement. Positive values indicate upward movement.
----@return self
-function MouseInteractions:wheelmoved(x, y) end
+---@return self T
+function MouseInteractions.wheelmoved(self, x, y) end
