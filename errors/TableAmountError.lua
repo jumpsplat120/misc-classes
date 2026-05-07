0bba7901fa4a638b433fc66d6ee0d6cab7035de7
jumpsplat120 <jumpsplat120@yahoo.com>
2026-05-07 03:16:26 -0700

    Rename TableAmountError to TableLengthError

--- errors/TableAmountError.lua
+++
@@ -1,3 +0,0 @@
-local Error = require("classes.Error")
-
-return Error("table_amount", "%s table values were expected, but only %s were received.")
---
+++ errors/TableLengthError.lua
@@ -0,0 +1,3 @@
+local Error = require("classes.Error")
+
+return Error("table_length", "%s table values were expected, but only %s were received.")
