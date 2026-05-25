af678a8e527e7ea66ab20c1af73916e6e17f8bc0
jumpsplat120 <jumpsplat120@yahoo.com>
2026-05-25 01:34:00 -0700

    fix grammar

--- errors/ParameterAmountError.lua
+++ errors/ParameterAmountError.lua
@@ -1,3 +1,3 @@
 local Error = require("classes.Error")
 
-return Error("parameter_amount", "%s arguments were expected, but only %s were received.")
+return Error("parameter_amount", "%s arguments were expected, but %s were received.")
