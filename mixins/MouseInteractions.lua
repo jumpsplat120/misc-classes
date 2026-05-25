f36adff00963fc079b630c8b0b4a834aaad3a79d
jumpsplat120 <jumpsplat120@yahoo.com>
2026-05-25 01:38:25 -0700

    remove Vector dependency, since contain no longer requires a vector

--- mixins/MouseInteractions.lua
+++ mixins/MouseInteractions.lua
@@ -1,11 +1,8 @@
 local Object, private
 local MouseInteractions
-local Vector
 
 Object  = require("lib.Classy")
 private = require("lib.Classy.instances")
-
-Vector = require("classes.Vector")
 
 ---@type MouseInteractions.Mixin
 MouseInteractions = Object:init()
@@ -23,8 +20,7 @@ function MouseInteractions:new()
             mouse_in    = false,
             down_on     = false,
             dragged_in  = false,
-            dragged_out = false,
-            position    = Vector:fromValues(0, 0)
+            dragged_out = false
         },
         current = {
             hold_down   = false,
@@ -32,7 +28,7 @@ function MouseInteractions:new()
             down_on     = false,
             dragged_in  = false,
             dragged_out = false,
-            position    = Vector:fromValues(0, 0)
+            position    = { 0, 0 }
         }
     }
 end
@@ -42,18 +38,19 @@ end
     --======METHODS======--
 
 function MouseInteractions:update(dt)
-    local p, touching
+    local p, x, y, touching
     
     p        = private[self]
-    touching = self:contains(p.mouse_interactions.current.position)
+    x, y     = table.unpack(p.mouse_interactions.current.position)
+    touching = self:contains(x, y)
 
     if touching then
-        self:dispatch("mouse.hover", p.mouse_interactions.current.position)
-        self:dispatchSync("mouse.hover", p.mouse_interactions.current.position)
+        self:dispatch("mouse.hover", x, y)
+        self:dispatchSync("mouse.hover", x, y)
 
         if p.mouse_interactions.previous.hold_down and p.mouse_interactions.current.hold_down then
-            self:dispatch("mouse.hold", p.mouse_interactions.current.position)
-            self:dispatchSync("mouse.hold", p.mouse_interactions.current.position)
+            self:dispatch("mouse.hold", x, y)
+            self:dispatchSync("mouse.hold", x, y)
         end
     end
 
@@ -69,9 +66,10 @@ function MouseInteractions:mousepressed(x, y, ...)
     
     p = private[self]
     
-    p.mouse_interactions.current.position:setToValues(x, y)
+    p.mouse_interactions.current.position[1] = x
+    p.mouse_interactions.current.position[2] = y
 
-    touching = self:contains(p.mouse_interactions.current.position)
+    touching = self:contains(x, y)
 
     p.mouse_interactions.previous.mouse_in = p.mouse_interactions.current.mouse_in
     p.mouse_interactions.current.mouse_in  = touching
@@ -98,9 +96,10 @@ function MouseInteractions:mousereleased(x, y, ...)
     
     p = private[self]
 
-    p.mouse_interactions.current.position:setToValues(x, y)
+    p.mouse_interactions.current.position[1] = x
+    p.mouse_interactions.current.position[2] = y
 
-    touching = self:contains(p.mouse_interactions.current.position)
+    touching = self:contains(x, y)
     
     if touching then
         self:dispatch("mouse.released", x, y, ...)
@@ -131,9 +130,10 @@ function MouseInteractions:mousemoved(x, y, ...)
     
     p = private[self]
 
-    p.mouse_interactions.current.position:setToValues(x, y)
+    p.mouse_interactions.current.position[1] = x
+    p.mouse_interactions.current.position[2] = y
 
-    touching = self:contains(p.mouse_interactions.current.position)
+    touching = self:contains(x, y)
     
     if touching then
         self:dispatch("mouse.over", x, y, ...)
@@ -152,11 +152,11 @@ function MouseInteractions:mousemoved(x, y, ...)
     end
 
     if not p.mouse_interactions.previous.mouse_in and touching then
-        self:dispatch("mouse.entered", p.mouse_interactions.current.position)
-        self:dispatchSync("mouse.entered", p.mouse_interactions.current.position)
+        self:dispatch("mouse.entered", x, y)
+        self:dispatchSync("mouse.entered", x, y)
     elseif p.mouse_interactions.previous.mouse_in and not touching then
-        self:dispatch("mouse.left", p.mouse_interactions.current.position)
-        self:dispatchSync("mouse.left", p.mouse_interactions.current.position)
+        self:dispatch("mouse.left", x, y)
+        self:dispatchSync("mouse.left", x, y)
     end
 
     p.mouse_interactions.previous.mouse_in = p.mouse_interactions.current.mouse_in
@@ -166,7 +166,7 @@ function MouseInteractions:mousemoved(x, y, ...)
 end
 
 function MouseInteractions:wheelmoved(...)
-    if not self:contains(private[self].mouse_interactions.current.position) then return self end
+    if not self:contains(table.unpack(private[self].mouse_interactions.current.position)) then return self end
 
     self:dispatch("mouse.scrollon", ...)
     self:dispatchSync("mouse.scrollon", ...)
