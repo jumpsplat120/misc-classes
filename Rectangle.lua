4678100f5b54a73e222d222d6c85adbffb4d981d
jumpsplat120 <jumpsplat120@yahoo.com>
2026-05-25 01:58:22 -0700

    reduce nearly all internal vector usage

--- Rectangle.lua
+++ Rectangle.lua
@@ -1,13 +1,10 @@
 local Object, private
 local Rectangle
-local Vector
 local MouseInteractions, Drawable, Emitter
 local ConstructorError, VectorSizeError, TypeError, InvalidError
 
 Object  = require("lib.Classy")
 private = require("lib.Classy.instances")
-
-Vector = require("classes.Vector")
 
 Emitter           = require("classes.mixins.Emitter")
 Drawable          = require("classes.mixins.Drawable")
@@ -31,6 +28,8 @@ valid_modes_lut = {
 
 valid_modes = table.join(valid_modes_lut, ", ", " and ")
 
+internal = math.random()
+
     --======STATIC======--
 
     --======CONSTRUCTOR======--
@@ -46,12 +45,12 @@ function Rectangle:fromValues(mode, x, y, width, height)
 
     InvalidError:assert(valid_modes_lut[mode], mode, "mode", valid_modes)
 
-    internal = math.uuid()
+    internal = math.random()
     
     return self {
         mode     = mode,
-        size     = Vector:fromValues(width, height),
-        position = Vector:fromValues(x, y),
+        size     = { width, height },
+        position = { x, y },
         internal = internal
     }
 end
@@ -68,12 +67,12 @@ function Rectangle:fromVectors(mode, position, size)
     VectorSizeError:assert(size.size == 2, size.size, 2)
     VectorSizeError:assert(position.size == 2, position.size, 2)
 
-    internal = math.uuid()
+    internal = math.random()
     
     return self {
         mode     = mode,
-        size     = size:clone(),
-        position = position:clone(),
+        size     = size.table,
+        position = position.table,
         internal = internal
     }
 end
@@ -89,41 +88,39 @@ function Rectangle:new(opts)
 
     p.mode   = opts.mode
     p.size   = opts.size
-    p.offset = Vector:fromValues(0, 0)
+    p.offset = { 0, 0 }
 
-    p.drawable.transform:translate(opts.position)
+    p.drawable.transform:translate(opts.position[1], opts.position[2])
 end
 
     --======METHODS======--
 
-function Rectangle:contains(vector)
-    local p, vx, vy
-    
-    p = private[self]
+function Rectangle:contains(x, y)
+    local p = private[self]
 
-    TypeError:assert(type(vector) == "vector", "vector", type(vector), "vector")
-    VectorSizeError:assert(vector.size == 2, vector.size, 2)
+    TypeError:assert(type(x) == "number", "x", type(x), "number")
+    TypeError:assert(type(y) == "number", "y", type(y), "number")
 
-    vx, vy = p.drawable.transform:inverseTransformValues(vector.x, vector.y)
+    x, y = p.drawable.transform:inverseTransform(x, y)
 
-    return vx >= p.offset.x and
-           vy >= p.offset.y and
-           vx <  p.offset.x + p.size.x and
-           vy <  p.offset.y + p.size.y
+    return x >= p.offset[1] and
+           y >= p.offset[2] and
+           x <  p.offset[1] + p.size[1] and
+           y <  p.offset[2] + p.size[2]
 end
 
 --TODO: Convert second rectangle to localspace of first rectangle, using inverseTransformValues
 --then do AABB
 function Rectangle:touching(rectangle)
-    local x, y, w, h, p, origin
-
-    p = private[self]
-    x, y, w, h = rectangle:unpack()
-
-    return p.offset.x + p.size.x >= x     and
-           p.offset.x            <= x + w and
-           p.offset.y + p.size.y >= y     and
-           p.offset.y            <= y + h
+    --local x, y, w, h, p, origin
+    --
+    --p = private[self]
+    --x, y, w, h = rectangle:unpack()
+    --
+    --return p.offset.x + p.size.x >= x     and
+    --       p.offset.x            <= x + w and
+    --       p.offset.y + p.size.y >= y     and
+    --       p.offset.y            <= y + h
 end
 
 function Rectangle:draw()
@@ -133,7 +130,7 @@ function Rectangle:draw()
 
     Drawable.apply(self)
     
-    love.graphics.rectangle(p.mode, p.offset.x, p.offset.y, p.size.x, p.size.y)
+    love.graphics.rectangle(p.mode, p.offset[1], p.offset[2], p.size[1], p.size[2])
 
     love.graphics.pop()
 
@@ -144,9 +141,10 @@ function Rectangle:clone()
     local p, rectangle
     
     p         = private[self]
-    rectangle = getmetatable(self):fromValues(p.mode, 0, 0, p.size.x, p.size.y)
+    rectangle = getmetatable(self):fromValues(p.mode, 0, 0, p.size[1], p.size[2])
 
-    rectangle.offset:setToVector(p.offset)
+    rectangle.ox = p.offset[1]
+    rectangle.oy = p.offset[2]
 
     rectangle.transform.matrix = self.transform.matrix
     
@@ -156,37 +154,29 @@ end
 function Rectangle:unpack()
     local p = private[self]
 
-    return p.offset.x, p.offset.y, p.size.x, p.size.y
+    return p.offset[1], p.offset[2], p.size[1], p.size[2]
 end
 
     --======GETTERS======--
 
 function Rectangle.__get:ox()
-    return private[self].offset.x
+    return private[self].offset[1]
 end
 
 function Rectangle.__get:oy()
-    return private[self].offset.y
+    return private[self].offset[2]
 end
 
 function Rectangle.__get:mode()
     return private[self].mode
 end
 
-function Rectangle.__get:size()
-    return private[self].size
-end
-
 function Rectangle.__get:width()
-    return private[self].size.x
+    return private[self].size[1]
 end
 
 function Rectangle.__get:height()
-    return private[self].size.y
-end
-
-function Rectangle.__get:offset()
-    return private[self].offset
+    return private[self].size[1]
 end
 
     --======SETTERS======--
@@ -213,13 +203,6 @@ function Rectangle.__set:mode(value)
     private[self].mode = value
 end
 
-function Rectangle.__set:size(value)
-    TypeError:assert(type(value) == "vector", "size", type(value), "vector")
-    VectorSizeError:assert(value.size == 2, value.size, 2)
-
-    private[self].size:setToVector(value)
-end
-
 function Rectangle.__set:width(value)
     TypeError:assert(type(value) == "number", "width", type(value), "number")
 
@@ -232,13 +215,6 @@ function Rectangle.__set:height(value)
     private[self].size.y = value
 end
 
-function Rectangle.__set:offset(value)
-    TypeError:assert(type(value) == "vector", "offset", type(value), "vector")
-    VectorSizeError:assert(value.size == 2, value.size, 2)
-
-    private[self].offset:setToVector(value)
-end
-
     --======METAMETHODS======--
 
 function Rectangle:__tostring()
@@ -246,8 +222,8 @@ function Rectangle:__tostring()
     
     p = private[self]
 
-    sx, sy = p.drawable.transform:transformValues(p.size.x, p.size.y)
-    ox, oy = p.drawable.transform:transformValues(p.offset.x, p.offset.y)
+    sx, sy = p.drawable.transform:transform(p.size[1], p.size[2])
+    ox, oy = p.drawable.transform:transform(p.offset[1], p.offset[2])
 
     return self:tostring(p.mode, ox, oy, sx, sy)
 end
