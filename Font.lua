6b6b3c3bdd18bbe2b50ecc3572d7fe67bee2e85e
jumpsplat120 <jumpsplat120@yahoo.com>
2026-06-22 00:33:09 -0700

    remove `is` and `TL` usuage

--- Font.lua
+++ Font.lua
@@ -1,14 +1,10 @@
 local Object, private
 local Font
-local is, TL
 local TypeError, InvalidError, FileTypeError, ConstructorError, GlyphRenderError
 local utf8
 
 Object  = require("lib.Classy")
 private = require("lib.Classy.instances")
-
-is = require("lib.is")
-TL = require("lib.string_template")
 
 TypeError        = require("classes.errors.TypeError")
 InvalidError     = require("classes.errors.InvalidError")
@@ -22,25 +18,33 @@ Font = Object:init()
 
     --======PRIVATE FUNCTIONS======--
 
-local symbols
+local symbols, internal
+local any, stringify
+
+internal = math.random()
 
 symbols = {
     hinting = table.join(table.keys(private[Font].hinting), ", "),
     filter  = table.join(table.keys(private[Font].filter), ", ")
 }
 
-local function convertToStrings(i, v)
-    if is(v, "number") then return i, utf8.char(v) end
-    if is(v, "string") then
-        if #v <= 1 then return i, v end
-        
-        return i, v:split()
+function stringify(i, v)
+    local T = type(v)
+
+    TypeError:assert(T == "string" or T == "number", "<...>[" .. tostring(i) .. "]", "string/number")
+    
+    if T == "number" then
+        return i, utf8.char(v)
     end
 
-    TypeError:throw(TL("args[%{i}]", { i = i }), type(v), "string/number")
+    if #v <= 1 then
+        return i, v
+    end
+    
+    return i, v:split()
 end
 
-local function anyTrue(_, a, b)
+function any(_, a, b)
     return a and b
 end
 
@@ -51,9 +55,9 @@ function Font:fromDefault(size, hinting, dpi)
     size    = size or 12
     dpi     = dpi or love.graphics.getDPIScale()
     
-    TypeError:assert(is(hinting, Symbol), "hinting", type(hinting), Symbol)
-    TypeError:assert(is(size, "number"), "size", type(size), "number")
-    TypeError:assert(is(dpi, "number"), "dpi", type(dpi), "number")
+    TypeError:assert(type(hinting) == "string", "hinting", type(hinting), "string")
+    TypeError:assert(type(size) == "number", "size", type(size), "number")
+    TypeError:assert(type(dpi) == "number", "dpi", type(dpi), "number")
 
     InvalidError:assert(private[Font].hinting[hinting], hinting, "hinting", symbols.hinting)
 
@@ -69,21 +73,21 @@ function Font:fromDefault(size, hinting, dpi)
 end
 
 function Font:fromTTF(font_path, size, hinting, dpi)
-    TypeError:assert(is(font_path, "string"), "font_path", type(font_path), "string")
+    TypeError:assert(type(font_path) == "string", "font_path", type(font_path), "string")
 
     hinting = hinting or NORMAL
     size    = size or 12
     dpi     = dpi or love.graphics.getDPIScale()
     
-    TypeError:assert(is(hinting, Symbol), "hinting", type(hinting), Symbol)
-    TypeError:assert(is(size, "number"), "size", type(size), "number")
-    TypeError:assert(is(dpi, "number"), "dpi", type(dpi), "number")
+    TypeError:assert(type(hinting) == "string", "hinting", type(hinting), "string")
+    TypeError:assert(type(size) == "number", "size", type(size), "number")
+    TypeError:assert(type(dpi) == "number", "dpi", type(dpi), "number")
 
     InvalidError:assert(private[Font].hinting[hinting], hinting, "hinting", symbols.hinting)
 
     internal = math.random()
 
-    return Font{
+    return self {
         font_path = font_path,
         internal  = internal,
         hinting   = hinting,
@@ -94,12 +98,12 @@ function Font:fromTTF(font_path, size, hinting, dpi)
 end
 
 function Font:fromBMF(font_path, image_path)
-    TypeError:assert(is(font_path, "string"), "font_path", type(font_path), "string")
-    TypeError:assert(is(image_path, "string"), "image_path", type(image_path), "string")
+    TypeError:assert(type(font_path) == "string", "font_path", type(font_path), "string")
+    TypeError:assert(type(image_path) == "string", "image_path", type(image_path), "string")
 
     internal = math.random()
 
-    return Font{
+    return self {
         image_path = image_path,
         font_path  = font_path,
         internal   = internal,
@@ -154,11 +158,11 @@ function Font:kerning(a, b)
     if a == "" then return 0 end
     if b == "" then return 0 end
 
-    if is(a, "number") then a = utf8.char(a) end
-    if is(b, "number") then b = utf8.char(b) end
+    if type(a) == "number" then a = utf8.char(a) end
+    if type(b) == "number" then b = utf8.char(b) end
 
-    TypeError:assert(is(a, "string"), "a", type(a), "string/number")
-    TypeError:assert(is(b, "string"), "b", type(b), "string/number")
+    TypeError:assert(type(a) == "string", "a", type(a), "string/number")
+    TypeError:assert(type(b) == "string", "b", type(b), "string/number")
 
     concat = a .. b
 
@@ -175,7 +179,7 @@ end
 function Font:width(text)
     local p = private[self]
 
-    TypeError:assert(is(text, "string"), "text", type(text), "string")
+    TypeError:assert(type(text) == "string", "text", type(text), "string")
     
     if text == "" then return 0 end
     
@@ -186,6 +190,8 @@ end
 
 --Take a string, and return a version of it that strips out all unrenderable characters.
 function Font:strip(text)
+    TypeError:assert(type(text) == "string", "text", type(text), "string")
+    
     for char, valid in pairs(self:canRender(text)) do
         if not valid then
             text = text:gsub(char, "")
@@ -203,7 +209,7 @@ function Font:canRender(...)
     return table.foreach(
         table.imerge(
             table.unpack(
-                table.foreach({ ... }, convertToStrings)
+                table.foreach({ ... }, stringify)
         )),
         function(_, v)
             if p.glyphs[v] ~= nil then
@@ -220,19 +226,19 @@ end
 --Pass in a string or multiple strings, and returns true only if the entire string
 --can be rendered.
 function Font:canRenderAll(...)
-    return table.reduce(table.values(self:canRender(...)), anyTrue)
+    return table.reduce(table.values(self:canRender(...)), any)
 end
 
 function Font:fallbacks(...)
-    local p, args
+    local p, args, index
 
     p    = private[self]
     args = { ... }
 
     for i, font in ipairs(args) do
-        local index = TL("args[%{i}]", { i = i })
+        index = "<...>[" .. tostring(i) .. "]"
 
-        TypeError:assert(is(font, Font), index, type(font), Font)
+        TypeError:assert(type(font) == "font", index, type(font), "font")
         FileTypeError:assert(font.type == p.type, index, font.type, p.type)
     end
 
@@ -242,39 +248,8 @@ function Font:fallbacks(...)
 end
 
 function Font:apply()
-    local p, prev
-    
-    p    = private[self]
-    prev = love.graphics.getFont()
+    love.graphics.setFont(private[self].font)
 
-    if p.temp         then return self end
-    if prev == p.font then return self end
-
-    p.temp = prev
-
-    love.graphics.setFont(p.font)
-
-    return self
-end
-
-function Font:remove()
-    local p, prev
-    
-    p    = private[self]
-    prev = love.graphics.getFont()
-
-    if not p.temp then return self end
-    
-    if prev ~= p.font then
-        p.temp = nil
-
-        return self
-    end
-
-    love.graphics.setFont(p.temp)
-
-    p.temp = nil
-    
     return self
 end
 
@@ -284,7 +259,7 @@ function Font:clone()
     internal = math.random()
 
     p    = private[self]
-    font = Font{
+    font = self {
         internal = internal,
         type = p.type,
         font_path = p.font_path,
@@ -387,7 +362,7 @@ function Font.__set:size(value)
         error("Unable to set BMF font size.")
     end
 
-    TypeError:assert(is(value, "number"), "size", type(value), "number")
+    TypeError:assert(type(value) == "number", "size", type(value), "number")
         
     p.size = value
     p.font = p.type == "ttf" and love.graphics.newFont(p.font_path, p.size, p.hinting.id, p.dpi) or
@@ -398,7 +373,7 @@ function Font.__set:hinting(value)
     local p = private[self]
 
     if p.type ~= "bmf" then
-        TypeError:assert(is(value, "number"), "hinting", type(value), "number")
+        TypeError:assert(type(value) == "number", "hinting", type(value), "number")
         
         p.hinting = value
         p.font    = love.graphics.newFont(p.font_path, p.size, p.hinting, p.dpi)
@@ -409,7 +384,7 @@ function Font.__set:dpi(value)
     local p = private[self]
 
     if p.type ~= "bmf" then
-        TypeError:assert(is(value, "number"), "dpi", type(value), "number")
+        TypeError:assert(type(value) == "number", "dpi", type(value), "number")
         
         p.dpi  = value
         p.font = love.graphics.newFont(p.font_path, p.size, p.hinting, p.dpi)
@@ -419,13 +394,13 @@ end
 function Font.__set:line_height(value)
     local p = private[self]
 
-    TypeError:assert(is(value, "number"), "line_height", type(value), "number")
+    TypeError:assert(type(value) == "number", "line_height", type(value), "number")
 
     p.font:setLineHeight(value)
 end
 
 function Font.__set:filter(value)
-    TypeError:assert(is(value, "table"), "filter", type(value), "table")
+    TypeError:assert(type(value) == "table", "filter", type(value), "table")
 
     local result = {}
 
@@ -437,7 +412,7 @@ function Font.__set:filter(value)
         end
 
         if k == "anisotropy" then
-            TypeError:assert(is(v, "number"), "filter.anisotropy", type(v), "number")
+            TypeError:assert(type(v) == "number", "filter.anisotropy", type(v), "number")
 
             result[k] = v
         end
@@ -451,7 +426,7 @@ end
 function Font.__set:min_filter(value)
     local p = private[self]
 
-    TypeError:assert(is(value, Symbol), "min_filter", type(value), Symbol)
+    TypeError:assert(type(value) == "string", "min_filter", type(value), "string")
     InvalidError:assert(private[Font].filter[value], value, "min_filter", symbols.filter)
 
     if not p.filter then
@@ -472,7 +447,7 @@ end
 function Font.__set:max_filter(value)
     local p = private[self]
 
-    TypeError:assert(is(value, Symbol), "max_filter", type(value), Symbol)
+    TypeError:assert(type(value) == "string", "max_filter", type(value), "string")
     InvalidError:assert(private[Font].filter[value], value, "max_filter", symbols.filter)
 
     if not p.filter then
@@ -493,7 +468,7 @@ end
 function Font.__set:filter_anisotropy(value)
     local p = private[self]
 
-    TypeError:assert(is(value, "number"), "filter_anisotropy", type(value), "number")
+    TypeError:assert(type(value) == "number", "filter_anisotropy", type(value), "number")
 
     if not p.filter then
         local min, max, anisotropy = p.font:getFilter()
@@ -513,11 +488,12 @@ end
     --======METAMETHODS======--
 
 function Font:__tostring()
-    if self.is_instance then return self:tostringHelper(private[self].name) end
-
-    return self:tostringHelper("Class")
+    return self:tostring(private[self].name)
 end
 
 Font.__type = "font"
 
-return Font
+---@type Font.Class
+local Class = Object:create(Font)
+
+return Class
